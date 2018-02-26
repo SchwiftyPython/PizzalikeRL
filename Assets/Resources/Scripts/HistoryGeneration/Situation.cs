517240bf72938bc839e87898d5af8bfcccab49c7
Brian Hanna-Sauro <bhannasauro@hotmail.com>
2018-02-25 21:12:41 -0500

    Revert "Revert "Added SituationContainer and SituationLoader.""
    This reverts commit 0da8636d8fec7d083b721b1e55eaa9252965c172.

--- Assets/Resources/Scripts/HistoryGeneration/Situation.cs
+++ Assets/Resources/Scripts/HistoryGeneration/Situation.cs
@@ -1,8 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
 
 public class Situation {
 
-    public string name;
+    [XmlAttribute("name")]
+    public string Name;
+
+    [XmlArray("Types")]
+    [XmlArrayItem("Type")]
+    public List<string> Types;
+
+    [XmlArray("Actors")]
+    [XmlArrayItem("Actor")]
+    public List<Entity> Actors;
+
+    [XmlElement("ConditionCode")]
+    public string ConditionCode;
+
+    [XmlArray("AllowedFactions")]
+    [XmlArrayItem("Faction")]
+    public List<Faction> AllowedFactions;
 }
