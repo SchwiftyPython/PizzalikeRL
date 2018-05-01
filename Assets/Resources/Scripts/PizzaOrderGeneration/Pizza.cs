0662d53bececf655e29ee1698de1c70b74a89270
Brian Hanna-Sauro <bhannasauro@hotmail.com>
2018-04-30 21:08:18 -0400

    Created Pizza and PizzaOrder classes.

---
+++ Assets/Resources/Scripts/PizzaOrderGeneration/Pizza.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+public class Pizza
+{
+    #region Enums
+
+    private enum Size
+    {
+        Personal,
+        Medium,
+        Large
+    }
+
+    private enum Crust
+    {
+        HandTossed,
+        Stuffed,
+        Thin
+    }
+
+    private enum Sauce
+    {
+        Marinara,
+        White,
+        Barbeque,
+        HotSauce
+    }
+
+    private enum Toppings
+    {
+        Pepperoni,
+        Sausage,
+        Meatball,
+        Ham,
+        Bacon,
+        Chicken,
+        Beef,
+        Pork,
+        Mushrooms,
+        Spinach,
+        Onion,
+        Olives,
+        BellPepper,
+        Pineapple,
+        Jalapeno
+    }
+
+    #endregion Enums
+
+    private Size _size;
+    private Crust _crust;
+    private Sauce _sauce;
+    private List<Toppings> _toppings;
+
+    public Pizza()
+    {
+        //TODO Generate pizza
+    }
+}
---
+++ Assets/Resources/Scripts/PizzaOrderGeneration/PizzaOrder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PizzaOrder
+{
+    public List<Pizza> Pizzas;
+    public Entity Customer;
+    
+    //TODO customer's location
+
+    public PizzaOrder()
+    {
+        //TODO generate pizzas for order
+        // Define difficulty for an order
+        // based on number of pizzas and 
+        // number of toppings
+
+        //TODO for customer, Pick either a named entity or make one 
+    }
+}
