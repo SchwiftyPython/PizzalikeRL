d7ad61f739d3e7c0fee32e17a42886167e9b0a43
Brian Hanna-Sauro <bhannasauro@hotmail.com>
2018-05-15 20:22:22 -0400

    Added main game window with tab for pizza orders window.

---
+++ Assets/Resources/Scripts/UI/GameMenuWindow.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMenuWindow : MonoBehaviour
+{
+    private GameObject _currentWindow;
+    private Button _currentWindowTab;
+
+    public GameObject MainWindow;
+    public GameObject PizzaOrderJournal;
+
+    public Button PizzaOrderJournalTab;
+
+    public void Show(GameObject window, Button tab)
+    {
+        Hide(_currentWindow, _currentWindowTab);
+        _currentWindow = window;
+        _currentWindowTab = tab;
+
+        var tabColor = _currentWindowTab.GetComponent<Image>().color;
+        tabColor.a = 0.5f;
+        _currentWindowTab.GetComponent<Image>().color = tabColor;
+
+        _currentWindow.SetActive(true);
+    }
+
+    public void Hide(GameObject window, Button tab)
+    {
+        var tabColor = tab.GetComponent<Image>().color;
+        tabColor.a = 0.5f;
+        tab.GetComponent<Image>().color = tabColor;
+        window.SetActive(false);
+    }
+
+    public void OnTabSelected(Button tab)
+    {
+        switch (tab.name)
+        {
+            case "Orders":
+                Show(PizzaOrderJournal, tab);
+                break;
+        }
+    }
+}
