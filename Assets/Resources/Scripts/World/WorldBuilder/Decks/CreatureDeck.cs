461b42e75487c7cf3ff8bba460e54d8c22fd810d
Brian Hanna-Sauro <bhannasauro@hotmail.com>
2018-10-29 20:14:30 -0400

    Added creaturedeck

---
+++ Assets/Resources/Scripts/World/WorldBuilder/Decks/CreatureDeck.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+public class CreatureDeck : Deck<Entity>
+{
+    private List<EntityTemplate> _availableCreatures;
+
+    public int CardIndex;
+
+    public override List<Entity> Cards { get; set; }
+
+    public CreatureDeck(BiomeType biomeType)
+    {
+        //todo get available creatures for biome
+        Size = 10;
+        CardIndex = 0;
+        Build();
+        Shuffle();
+    }
+
+    public override void Build()
+    {
+        //todo loop through available creatures until deck full
+    }
+
+    public override Entity Draw()
+    {
+        var card = Cards[CardIndex];
+        if (CardIndex >= Size)
+        {
+            Shuffle();
+            CardIndex = 0;
+        }
+        else
+        {
+            CardIndex++;
+        }
+        return card;
+    }
+}
