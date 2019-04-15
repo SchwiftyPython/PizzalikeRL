70a47b0e81d36e8a66294fce8be3f2bfd072027f
Brian Hanna-Sauro <bhannasauro@hotmail.com>
2019-04-15 08:05:00 -0400

    Added logic for tabs and skill window.

--- Assets/Resources/Scripts/UI/DelivererSelectWindow.cs
+++ Assets/Resources/Scripts/UI/DelivererSelectWindow.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.U2D;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
@@ -13,14 +11,28 @@ public class DelivererSelectWindow : MonoBehaviour
     private const int MinDescendants = 2;
     private const int MaxDescendants = 4;
 
+    public Color ActiveTabColor;
+    public Color InactiveTabColor;
+
     public GameObject DescendantPrefab;
     public GameObject DescendantButtonParent;
-    public GameObject DescendantDescription;
 
     public GameObject StrengthValueBox;
     public GameObject AgilityValueBox;
     public GameObject ConstitutionValueBox;
     public GameObject IntelligenceValueBox;
+    public GameObject HpValueBox;
+    public GameObject DefenseValueBox;
+    public GameObject SpeedValueBox;
+
+    public GameObject StatsWindow;
+    public GameObject SkillsWindow;
+
+    public GameObject StatsTab;
+    public GameObject SkillsTab;
+
+    public GameObject CurrentWindow;
+    public Button CurrentWindowTab;
 
     public static DelivererSelectWindow Instance;
 
@@ -34,16 +46,25 @@ public class DelivererSelectWindow : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        StatsWindow.SetActive(false);
+        SkillsWindow.SetActive(false);
+
+        StatsTab.GetComponent<Image>().color = InactiveTabColor;
+        SkillsTab.GetComponent<Image>().color = InactiveTabColor;
+
+        ShowInnerWindow(CurrentWindow, CurrentWindowTab);
     }
 
     private void Start()
     {
-        _descendants = new Dictionary<Guid, Entity>();
         PopulateWindow();
     }
 
     private void PopulateWindow()
     {
+        _descendants = new Dictionary<Guid, Entity>();
+
         var numDescendants = Random.Range(MinDescendants, MaxDescendants + 1);
 
         for (var i = 0; i < numDescendants; i++)
@@ -60,6 +81,38 @@ public class DelivererSelectWindow : MonoBehaviour
 
             var playerSprite = descendantButton.GetComponentsInChildren<Image>()[1];
             playerSprite.sprite = descendant.GetSpritePrefab().GetComponent<SpriteRenderer>().sprite;
+        }
+    }
+
+    public void ShowInnerWindow(GameObject window, Button tab)
+    {
+        if (window != CurrentWindow)
+        {
+            HideInnerWindow(CurrentWindow, CurrentWindowTab);
+            CurrentWindow = window;
+            CurrentWindowTab = tab;
+        }
+        CurrentWindowTab.GetComponent<Image>().color = ActiveTabColor;
+
+        CurrentWindow.SetActive(true);
+    }
+
+    public void HideInnerWindow(GameObject window, Button tab)
+    {
+        tab.GetComponent<Image>().color = InactiveTabColor;
+        window.SetActive(false);
+    }
+
+    public void OnTabSelected(Button tab)
+    {
+        switch (tab.name)
+        {
+            case "Stats":
+                ShowInnerWindow(StatsWindow, tab);
+                break;
+            case "Skills":
+                ShowInnerWindow(SkillsWindow, tab);
+                break;
         }
     }
 }
