b421f1f172ee971d43c97ef12c08b41cabffc330
Brian Hanna-Sauro <bhannasauro@hotmail.com>
2019-06-28 07:55:46 -0400

    Populating settlements is working. Just need more citizens to place. Was missing vertical versions of doors. Made some okay ones for now.

--- Assets/Resources/Scripts/Messenger.cs
+++ Assets/Resources/Scripts/Messenger.cs
@@ -27,7 +27,6 @@ public class Messenger : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        DontDestroyOnLoad(gameObject);
 
         _messagesOnScreen = new Queue<GameObject>();
     }
