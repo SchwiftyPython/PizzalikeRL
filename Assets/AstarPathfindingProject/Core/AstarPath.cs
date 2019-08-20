750121c14f2fa59d72cb397eb1f3d2324ae4ca93
brian sauro <bhannasauro@hotmail.com>
2019-08-20 12:53:16 -0400

    removed debug.log may be editor related

--- Assets/AstarPathfindingProject/Core/AstarPath.cs
+++ Assets/AstarPathfindingProject/Core/AstarPath.cs
@@ -1582,9 +1582,10 @@ public class AstarPath : VersionedMonoBehaviour {
 
 		System.GC.Collect();
 
-		if (logPathResults != PathLog.None && logPathResults != PathLog.OnlyErrors) {
-			Debug.Log("Scanning - Process took "+(lastScanTime*1000).ToString("0")+" ms to complete");
-		}
+//        if (logPathResults != PathLog.None && logPathResults != PathLog.OnlyErrors)
+//        {
+//            Debug.Log("Scanning - Process took " + (lastScanTime * 1000).ToString("0") + " ms to complete");
+//        }
 	}
 
 	IEnumerable<Progress> ScanGraph (NavGraph graph) {
