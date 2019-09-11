4499a0e751190254a0f03da5a50c652823241ca2
brian sauro <bhannasauro@hotmail.com>
2019-09-11 06:36:37 -0400

    Added Effect base class Added Healing effect

---
+++ Assets/Resources/Scripts/Effects/Effect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Effect
+{
+    protected string _name;
+
+    protected Guid _id;
+
+    protected int _duration;
+
+    public string Name
+    {
+        get
+        {
+            if (_name != null && !_name.Equals(string.Empty))
+            {
+                return _name;
+            }
+
+            var fullName = GetType().FullName;
+
+            if (fullName != null)
+            {
+                _name = fullName.Substring(fullName.LastIndexOf('.') + 1);
+            }
+
+            return _name;
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/Effects/Healing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Healing : Effect
+{
+    private int _amountToHealPerTurn;
+
+    public Healing(int duration, int amount)
+    {
+        base._duration = duration;
+        base._name = "healing";
+        _amountToHealPerTurn = amount;
+    }
+}
