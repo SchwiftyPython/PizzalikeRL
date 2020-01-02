96d5bf70ef34f9c809ebd34e075913e18cb4fc6c
brian sauro <bhannasauro@hotmail.com>
2020-01-02 07:46:17 -0500

    Added ability select popup window

---
+++ Assets/Resources/Scripts/UI/AbilitySelectPopup.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilitySelectPopup : MonoBehaviour, ISubscriber
+{
+    public GameObject AvailableAbilityPrefab;
+    public GameObject AbilityCategoryPrefab;
+    public RectTransform AbilityCategoryParent;
+    public GameObject AbilityDescription; //todo make tooltip
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void PopulateAllAbilities()
+    {
+
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+        GameManager.Instance.RemoveActiveWindow(gameObject);
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+        GameManager.Instance.AddActiveWindow(gameObject);
+    }
+
+    public void OnNotify(string eventName, object broadcaster, object parameter = null)
+    {
+        throw new System.NotImplementedException();
+    }
+}
